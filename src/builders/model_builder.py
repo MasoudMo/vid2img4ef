d33164eb4959b042e2011f95d5953bc276d172a8
MasoudMo <masoudmokht@gmail.com>
2022-11-25 22:18:06 -0800

    Added support for freezing encoder weights during EF prediction

--- src/builders/model_builder.py
+++ src/builders/model_builder.py
@@ -39,10 +39,15 @@ def build(config,
                                  config['init_gain'],
                                  config['gpu_ids'])
     else:
+
         model['encoder'] = init_net(Conv3DEncoder(**config['generator']['encoder']),
                                     config['init_type'],
                                     config['init_gain'],
                                     config['gpu_ids'])
+        freeze_encoder = config['regressor'].pop('freeze_encoder')
+        if freeze_encoder:
+            for param in model['encoder'].parameters():
+                param.requires_grad = False
 
         regressor_name = config['regressor'].pop('type')
         model['regressor'] = init_net(REGRESSORS[regressor_name](**config['regressor']),
